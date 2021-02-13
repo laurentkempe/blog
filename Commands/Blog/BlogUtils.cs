19f4eae161ad15484d4692994f06594187693c07
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2021-02-13 11:57:24 +0100

    Update BlogOps with new publish

--- Commands/Blog/BlogUtils.cs
+++ Commands/Blog/BlogUtils.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
+using System.Threading.Tasks;
+using BlogOps.Commands.Utils;
+using Spectre.Console;
 
 namespace BlogOps.Commands.Blog
 {
@@ -17,5 +22,48 @@ namespace BlogOps.Commands.Blog
 
         public static bool IsDateLine(this string contentLine) => contentLine.StartsWith(Date);
         public static string ToDate(this DateTime date) => $"{Date} {date.ToString(CultureInfo.InvariantCulture)}";
+
+        public static async IAsyncEnumerable<(FileInfo fileInfo, BlogFrontMatter draftFrontMatter)> GetDraftInfos()
+        {
+            foreach (var draftPath in Directory.GetFiles(BlogSettings.DraftsFolder))
+            {
+                yield return await GetDraftInfo(draftPath);
+            }            
+        }
+
+        public static async Task<(FileInfo fileInfo, BlogFrontMatter draftFrontMatter)> AskUserToSelectDraft(string userQuestion)
+        {
+            var drafts = new Dictionary<string, (FileInfo fileInfo, BlogFrontMatter draftFrontMatter)>();
+
+            await foreach (var draftInfo in GetDraftInfos())
+            {
+                drafts.Add(draftInfo.draftFrontMatter.Title, draftInfo);
+            }
+
+            var draft = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title(userQuestion)
+                    .PageSize(10)
+                    .AddChoices(drafts.Keys));
+
+            AnsiConsole.WriteLine($"You selected '{draft}'!");
+
+            return drafts[draft];
+        }
+
+        private static async Task<(FileInfo fileInfo, BlogFrontMatter draftFrontMatter)> GetDraftInfo(string draftPath)
+        {
+            var fileInfo = new FileInfo(draftPath);
+            var draftFrontMatter = await GetDraftFrontMatter(draftPath);
+
+            return (fileInfo, draftFrontMatter);
+        }
+
+        private static async Task<BlogFrontMatter> GetDraftFrontMatter(string draftPath)
+        {
+            var allText = await File.ReadAllTextAsync(draftPath);
+
+            return allText.GetFrontMatter<BlogFrontMatter>();
+        }
     }
 }
---
+++ Commands/EditCommand.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using BlogOps.Commands.Blog;
+using CliFx;
+using CliFx.Attributes;
+using JetBrains.Annotations;
+using SimpleExec;
+
+namespace BlogOps.Commands
+{
+    [Command("edit", Description = "Edit a drafts blog post.")]
+    [UsedImplicitly]
+    public class EditCommand : ICommand
+    {
+        public async ValueTask ExecuteAsync(IConsole console)
+        {
+            var (fileInfo, _) = await BlogUtils.AskUserToSelectDraft("Which draft do you want to edit?");
+
+            await Command.RunAsync("cmd.exe", $"/c code {fileInfo.FullName}", "./");
+        }
+    }
+}
